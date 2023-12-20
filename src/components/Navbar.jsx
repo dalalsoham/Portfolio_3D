101c052d61718d8de4226af289480cd27d6a6c99
mr_sd_jack_003 <barson0habra@gmail.com>
2023-12-21 00:19:23 +0530

    update 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,14 @@ function Navbar() {
       <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
         <p className="blue-gradient_text">SD</p>
       </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" classname={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            About
+        </NavLink>
+        <NavLink to="/projects" classname={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+            Projects
+        </NavLink>
+      </nav>
     </header>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Section classname="w-full h-screen relative">
+
+    </Section>
+  )
+}
+
+export default Home
