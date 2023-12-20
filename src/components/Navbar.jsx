f028be376cd535692ec9ccd480953f83218be7fe
mr_sd_jack_003 <barson0habra@gmail.com>
2023-12-20 23:23:56 +0530

    setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">SD</p>
+      </NavLink>
+    </header>
+  )
+}
+
+export default Navbar
